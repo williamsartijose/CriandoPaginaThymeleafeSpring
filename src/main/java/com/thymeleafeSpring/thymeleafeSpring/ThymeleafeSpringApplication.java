c204f9085e45925383a7495d114af07392ee30df
unknown <williamsartijose@hotmail.com>
2022-12-19 12:30:09 +0000

    first commit

---
+++ src/main/java/com/thymeleafeSpring/thymeleafeSpring/ThymeleafeSpringApplication.java
@@ -0,0 +1,13 @@
+package com.thymeleafeSpring.thymeleafeSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeleafeSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThymeleafeSpringApplication.class, args);
+	}
+
+}
